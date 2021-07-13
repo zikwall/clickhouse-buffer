30c207320c068f91a8333cc1ffb218ea14cb333f
zikwall <andrey.kapitonov.96@gmail.com>
2021-07-13 18:19:21 +0300

    Add integration test

--- go.mod
+++ go.mod
@@ -4,8 +4,18 @@ go 1.16
 
 require (
 	github.com/ClickHouse/clickhouse-go v1.4.5
-	github.com/go-redis/redis/v8 v8.10.0 // indirect
+	github.com/Microsoft/go-winio v0.5.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.1.1 // indirect
+	github.com/containerd/continuity v0.1.0 // indirect
+	github.com/go-redis/redis/v8 v8.10.0
 	github.com/jmoiron/sqlx v1.2.0
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/opencontainers/runc v1.0.0 // indirect
+	github.com/ory/dockertest/v3 v3.7.0
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	go.opentelemetry.io/otel/internal/metric v0.21.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ integration_test.go
@@ -0,0 +1,59 @@
+// +build integration
+
+package clickhousebuffer
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"github.com/ory/dockertest/v3"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	var db *redis.Client
+
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	resource, err := pool.Run("redis", "6.2", nil)
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	if err := pool.Retry(func() error {
+		db = redis.NewClient(&redis.Options{
+			Addr: fmt.Sprintf("localhost:%s", resource.GetPort("6379/tcp")),
+		})
+
+		return db.Ping(db.Context()).Err()
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	code := m.Run()
+
+	// You can't defer this because os.Exit doesn't care for defer
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("Could not purge resource: %s", err)
+	}
+
+	if err := db.Set(context.Background(), "test_key", "test_value", 10*time.Second).Err(); err != nil {
+		log.Fatalf("Could not set value: %s", err)
+	}
+
+	if value := db.Get(context.Background(), "test_key").Val(); value != "test_value" {
+		log.Fatalf("Could not get correct value, received: %s", value)
+	}
+
+	os.Exit(code)
+}
+
+func TestSomething(t *testing.T) {
+	// db.Query()
+}
