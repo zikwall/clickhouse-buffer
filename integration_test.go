17d5b33e84bd3eed78a479dd8a33612a75c1515f
zikwall <andrey.kapitonov.96@gmail.com>
2021-07-13 20:36:14 +0300

    Review integration tests

--- integration_test.go
+++ integration_test.go
@@ -4,6 +4,7 @@ package clickhousebuffer
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"github.com/jmoiron/sqlx"
@@ -37,7 +38,7 @@ func TestMain(m *testing.M) {
 	var ch *sqlx.DB
 	var clickhouse Clickhouse
 
-	// STEP 1: Create Redis serrvice
+	// STEP 1: Create Redis service
 	pool, err := dockertest.NewPool("")
 	if err != nil {
 		log.Fatalf("Could not connect to redis docker: %s", err)
@@ -87,93 +88,30 @@ func TestMain(m *testing.M) {
 	}
 
 	// STEP 3: Drop and Create table under certain conditions
-	_, _ = ch.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %s", integrationTableName))
-	_, err = ch.Exec(fmt.Sprintf(`
-		CREATE TABLE IF NOT EXISTS %s (
-			id        	UInt8,
-			uuid   		String,
-			insert_ts   String
-		) engine=Memory
-	`, integrationTableName))
-
-	if err != nil {
-		log.Fatalf("Could create clickhouse table: %s", err)
+	if beforeCheckTables(ctx, ch); err != nil {
+		log.Fatal(err)
 	}
 
 	// STEP 4: Create clickhouse client and buffer writer with redis buffer
-	client := NewClientWithOptions(ctx, clickhouse,
-		DefaultOptions().SetFlushInterval(500).SetBatchSize(6),
-	)
+	client, redisBuffer, err := useClientAndRedisBuffer(ctx, clickhouse, db)
 
-	redisBuffer, err := redis2.NewBuffer(ctx, db, "bucket", client.Options().BatchSize())
 	if err != nil {
-		log.Fatalf("Could not create redis buffer: %s", err)
+		log.Fatal(err)
 	}
-
-	writeAPI := client.Writer(View{
-		Name:    integrationTableName,
-		Columns: []string{"id", "uuid", "insert_ts"},
-	}, redisBuffer)
 
 	// STEP 5: Write own data to redis
-	writeAPI.WriteRow(integrationRow{
-		id: 1, uuid: "1", insertTS: time.Now(),
-	})
-	writeAPI.WriteRow(integrationRow{
-		id: 2, uuid: "2", insertTS: time.Now(),
-	})
-	writeAPI.WriteRow(integrationRow{
-		id: 3, uuid: "3", insertTS: time.Now(),
-	})
-	writeAPI.WriteRow(integrationRow{
-		id: 4, uuid: "4", insertTS: time.Now(),
-	})
-	writeAPI.WriteRow(integrationRow{
-		id: 5, uuid: "5", insertTS: time.Now(),
-	})
+	writeDataToBuffer(client, redisBuffer)
 
 	// STEP 6: Checks!
-
-	// wait a bit
-	<-time.After(50 * time.Millisecond)
-
-	// try read from redis buffer before flushing data in storage (Redis)
-	rows := redisBuffer.Read()
-	if len(rows) != 5 {
-		log.Fatalf("Could not get correct valuse, received: %v", rows)
-	}
-	log.Printf("Received value: %v", rows)
-
-	// wait until flush in redis buffer
-	<-time.After(500 * time.Millisecond)
-
-	// check redis buffer size
-	if size := redisBuffer.Len(); size != 0 {
-		log.Fatal("Failed, the buffer was expected to be cleared")
+	if err := checksBuffer(redisBuffer); err != nil {
+		log.Fatal(err)
 	}
 
-	// check data in clickhouse, write after flushing
-	values, err := fetchClickhouseRows(ctx, ch)
-	if err != nil {
-		log.Fatalf("Could not fetch data from clickhouse: %s", err)
-	}
-
-	log.Printf("Received values from clickhouse table: %v", values)
-
-	if len(values) != 5 {
-		log.Fatalf("Failed, expected to get five values, received %d", len(values))
-	}
-
-	if v := values[2].id; v != 3 {
-		log.Fatalf("Failed, expected value 3, received %d", v)
-	}
-
-	if v := values[2].uuid; v != "3" {
-		log.Fatalf("Failed, expected value 3, received %s", v)
+	if err := checksClickhouse(ctx, ch); err != nil {
+		log.Fatal(err)
 	}
 
 	// STEP 7: Close resources
-
 	code := m.Run()
 
 	// You can't defer this because os.Exit doesn't care for defer
@@ -221,6 +159,104 @@ func fetchClickhouseRows(ctx context.Context, ch *sqlx.DB) ([]clickhouseRowData,
 	}
 
 	return values, err
+}
+
+func beforeCheckTables(ctx context.Context, ch *sqlx.DB) error {
+	_, _ = ch.ExecContext(ctx, fmt.Sprintf("DROP TABLE IF EXISTS %s", integrationTableName))
+	_, err := ch.ExecContext(ctx, fmt.Sprintf(`
+		CREATE TABLE IF NOT EXISTS %s (
+			id        	UInt8,
+			uuid   		String,
+			insert_ts   String
+		) engine=Memory
+	`, integrationTableName))
+
+	return err
+}
+
+func useClientAndRedisBuffer(ctx context.Context, clickhouse Clickhouse, db *redis.Client) (Client, buffer.Buffer, error) {
+	client := NewClientWithOptions(ctx, clickhouse,
+		DefaultOptions().SetFlushInterval(500).SetBatchSize(6),
+	)
+
+	buf, err := redis2.NewBuffer(ctx, db, "bucket", client.Options().BatchSize())
+
+	if err != nil {
+		return nil, nil, fmt.Errorf("Could not create redis buffer: %s", err)
+	}
+
+	return client, buf, nil
+}
+
+func writeDataToBuffer(client Client, buf buffer.Buffer) {
+	writeAPI := client.Writer(View{
+		Name:    integrationTableName,
+		Columns: []string{"id", "uuid", "insert_ts"},
+	}, buf)
+
+	writeAPI.WriteRow(integrationRow{
+		id: 1, uuid: "1", insertTS: time.Now(),
+	})
+	writeAPI.WriteRow(integrationRow{
+		id: 2, uuid: "2", insertTS: time.Now(),
+	})
+	writeAPI.WriteRow(integrationRow{
+		id: 3, uuid: "3", insertTS: time.Now(),
+	})
+	writeAPI.WriteRow(integrationRow{
+		id: 4, uuid: "4", insertTS: time.Now(),
+	})
+	writeAPI.WriteRow(integrationRow{
+		id: 5, uuid: "5", insertTS: time.Now(),
+	})
+
+	// wait a bit
+	<-time.After(50 * time.Millisecond)
+}
+
+func checksBuffer(buf buffer.Buffer) error {
+	// try read from redis buffer before flushing data in storage (Redis)
+	rows := buf.Read()
+
+	if len(rows) != 5 {
+		return fmt.Errorf("Could not get correct valuse, received: %v", rows)
+	}
+
+	log.Printf("Received value from buffer: %v", rows)
+
+	// wait until flush in redis buffer
+	<-time.After(500 * time.Millisecond)
+
+	// check redis buffer size
+	if size := buf.Len(); size != 0 {
+		errors.New("Failed, the buffer was expected to be cleared")
+	}
+
+	return nil
+}
+
+func checksClickhouse(ctx context.Context, ch *sqlx.DB) error {
+	// check data in clickhouse, write after flushing
+	values, err := fetchClickhouseRows(ctx, ch)
+	if err != nil {
+		fmt.Errorf("Could not fetch data from clickhouse: %s", err)
+	}
+
+	log.Printf("Received values from clickhouse table: %v", values)
+
+	if len(values) != 5 {
+		fmt.Errorf("Failed, expected to get five values, received %d", len(values))
+	}
+
+	if v := values[2].id; v != 3 {
+		fmt.Errorf("Failed, expected value 3, received %d", v)
+	}
+
+	if v := values[2].uuid; v != "3" {
+		fmt.Errorf("Failed, expected value 3, received %s", v)
+	}
+
+	return nil
 }
 
 func TestSomething(t *testing.T) {
