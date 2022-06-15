d7b7a7eaef395173d65beaffe016e3154d0886fb
Andrey Kapitonov <andrey.kapitonov.96@gmail.com>
2022-06-15 09:09:33 +0300

    Advanced: add more examples (#27)

---
+++ example/cmd/advanced/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go/v2"
+	"github.com/google/uuid"
+
+	clickhousebuffer "github.com/zikwall/clickhouse-buffer/v3"
+	"github.com/zikwall/clickhouse-buffer/v3/example/pkg/tables"
+	"github.com/zikwall/clickhouse-buffer/v3/src/buffer/cxmem"
+	"github.com/zikwall/clickhouse-buffer/v3/src/cx"
+	"github.com/zikwall/clickhouse-buffer/v3/src/db/cxnative"
+)
+
+func main() {
+	hostname := os.Getenv("CLICKHOUSE_HOST")
+	username := os.Getenv("CLICKHOUSE_USER")
+	database := os.Getenv("CLICKHOUSE_DB")
+	password := os.Getenv("CLICKHOUSE_PASS")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	ch, conn, err := cxnative.NewClickhouse(ctx, &clickhouse.Options{
+		Addr: []string{hostname},
+		Auth: clickhouse.Auth{
+			Database: database,
+			Username: username,
+			Password: password,
+		},
+		Settings: clickhouse.Settings{
+			"max_execution_time": 60,
+		},
+		DialTimeout: 5 * time.Second,
+		Compression: &clickhouse.Compression{
+			Method: clickhouse.CompressionLZ4,
+		},
+		Debug: true,
+	})
+	if err != nil {
+		log.Panicln(err)
+	}
+	if err := tables.CreateAdvancedTableNative(ctx, conn); err != nil {
+		log.Panicln(err)
+	}
+
+	client := clickhousebuffer.NewClientWithOptions(ctx, ch,
+		clickhousebuffer.DefaultOptions().SetDebugMode(true).SetFlushInterval(1000).SetBatchSize(10),
+	)
+	writeAPI := client.Writer(
+		cx.NewView(tables.AdvancedTableName(), tables.AdvancedTableColumns()),
+		cxmem.NewBuffer(client.Options().BatchSize()),
+	)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		errorsCh := writeAPI.Errors()
+		for err := range errorsCh {
+			log.Printf("clickhouse write error: %s\n", err.Error())
+		}
+		wg.Done()
+	}()
+
+	write(writeAPI)
+
+	<-time.After(time.Second * 2)
+	client.Close()
+	wg.Wait()
+}
+
+// nolint:gocritic // it's OK
+func write(writeAPI clickhousebuffer.Writer) {
+	for i := 0; i < 50; i++ {
+		writeAPI.WriteRow(&tables.AdvancedTable{
+			Col1: uint8(42),
+			Col2: "ClickHouse",
+			Col3: "Inc",
+			Col4: uuid.New(),
+			// Map(String, UInt8)
+			// Col5: map[string]uint8{"key": 1},
+			// Array(String)
+			Col6: []string{"Q", "W", "E", "R", "T", "Y"},
+			// Tuple(String, UInt8, Array(Map(String, String)))
+			Col7: []interface{}{
+				"String Value", uint8(5), []string{"val1", "val2", "val3"}, []interface{}{
+					time.Now(),
+					uint32(5),
+				},
+			},
+			Col8:  time.Now(),
+			Col9:  "hello",
+			Col10: time.Now(),
+			Col11: b2i8(i%2 == 0),
+			Col12: time.Now(),
+			Col13: [][]interface{}{
+				{
+					"String Value", uint8(5), []string{"val1", "val2", "val3"}, []interface{}{
+						time.Now(),
+						uint32(5),
+					},
+				},
+				{
+					"String Value", uint8(5), []string{"val1", "val2", "val3"}, []interface{}{
+						time.Now(),
+						uint32(5),
+					},
+				},
+			},
+		})
+	}
+}
+
+// clickhouse doesn't support bool
+func b2i8(b bool) int8 {
+	if b {
+		return 1
+	}
+	return 0
+}
---
+++ example/pkg/tables/advanced.go
@@ -0,0 +1,88 @@
+package tables
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+	"github.com/google/uuid"
+
+	"github.com/zikwall/clickhouse-buffer/v3/src/cx"
+)
+
+type AdvancedTable struct {
+	Col1 uint8
+	Col2 string
+	Col3 string
+	Col4 uuid.UUID
+	// Map type is not allowed. Set 'allow_experimental_map_type = 1'
+	// Col5 map[string]uint8
+	Col6  []string
+	Col7  []interface{}
+	Col8  time.Time
+	Col9  string
+	Col10 time.Time
+	Col11 int8
+	Col12 time.Time
+	Col13 [][]interface{}
+}
+
+func (a *AdvancedTable) Row() cx.Vector {
+	return cx.Vector{
+		a.Col1, a.Col2, a.Col3, a.Col4, a.Col6, a.Col7, a.Col8, a.Col9, a.Col10, a.Col11, a.Col12, a.Col13,
+	}
+}
+
+func AdvancedTableName() string {
+	return "default.advanced_example"
+}
+
+func AdvancedTableColumns() []string {
+	return []string{"Col1", "Col2", "Col3", "Col4", "Col6", "Col7", "Col8", "Col9", "Col10", "Col11", "Col12", "Col13"}
+}
+
+// SELECT DISTINCT alias_to
+// FROM system.data_type_families
+//
+// ┌─alias_to────┐
+// │             │
+// │ IPv6        │
+// │ IPv4        │
+// │ FixedString │
+// │ String      │
+// │ Float64     │
+// │ UInt8       │
+// │ UInt16      │
+// │ DateTime    │
+// │ Decimal     │
+// │ UInt32      │
+// │ Int8        │
+// │ Int16       │
+// │ Int32       │
+// │ Int64       │
+// │ UInt64      │
+// │ Float32     │
+// └─────────────┘
+//
+// 17 rows in set. Elapsed: 0.028 sec.
+var createAdvancedTableQuery = fmt.Sprintf(`
+		CREATE TABLE IF NOT EXISTS %s (
+			  Col1 UInt8
+			, Col2 String
+			, Col3 FixedString(3)
+			, Col4 UUID
+			, Col6 Array(String)
+			, Col7 Tuple(String, UInt8, Array(String), Tuple(DateTime, UInt32))
+			, Col8 DateTime
+			, Col9 Enum('hello' = 1, 'world' = 2)
+			, Col10 DateTime64
+			, Col11 Bool
+			, Col12 Date
+			, Col13 Array(Tuple(String, UInt8, Array(String), Tuple(DateTime, UInt32)))
+		) Engine = Memory
+`, AdvancedTableName())
+
+func CreateAdvancedTableNative(ctx context.Context, conn driver.Conn) error {
+	return conn.Exec(ctx, createAdvancedTableQuery)
+}
