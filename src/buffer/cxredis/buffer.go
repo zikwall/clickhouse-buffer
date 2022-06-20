1ba4147527cdfc0a8cc0f187bc4820a5b393dbc4
zikwall <andrey.kapitonov.96@gmail.com>
2022-06-20 15:58:49 +0300

    Improve redis buffer

--- src/buffer/cxredis/buffer.go
+++ src/buffer/cxredis/buffer.go
@@ -2,24 +2,29 @@ package cxredis
 
 import (
 	"log"
+	"sync/atomic"
 
 	"github.com/zikwall/clickhouse-buffer/v3/src/cx"
 )
 
 func (r *redisBuffer) Write(row cx.Vector) {
-	buf, err := row.Encode()
-	if err == nil {
-		err = r.client.RPush(r.context, r.bucket, buf).Err()
-		if err != nil && !r.isContextClosedErr(err) {
+	var err error
+	var buf []byte
+	if buf, err = row.Encode(); err != nil {
+		log.Printf("redis buffer value encode err: %v\n", err.Error())
+		return
+	}
+	if err = r.client.RPush(r.context, r.bucket, buf).Err(); err != nil {
+		if !r.isContextClosedErr(err) {
 			log.Printf("redis buffer write err: %v\n", err.Error())
 		}
-	} else {
-		log.Printf("redis buffer value encode err: %v\n", err.Error())
+		return
 	}
+	atomic.AddInt64(&r.size, 1)
 }
 
 func (r *redisBuffer) Read() []cx.Vector {
-	values := r.client.LRange(r.context, r.bucket, 0, r.bufferSize).Val()
+	values := r.client.LRange(r.context, r.bucket, 0, atomic.LoadInt64(&r.size)).Val()
 	slices := make([]cx.Vector, 0, len(values))
 	for _, value := range values {
 		if v, err := cx.VectorDecoded(value).Decode(); err == nil {
@@ -32,9 +37,10 @@ func (r *redisBuffer) Read() []cx.Vector {
 }
 
 func (r *redisBuffer) Len() int {
-	return int(r.client.LLen(r.context, r.bucket).Val())
+	return int(r.size)
 }
 
 func (r *redisBuffer) Flush() {
 	r.client.LTrim(r.context, r.bucket, r.bufferSize, -1).Val()
+	atomic.CompareAndSwapInt64(&r.size, r.size, 0)
 }
